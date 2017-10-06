8d2e43299b91d5303928bb47fc3bf6d2fbf726da
parlind <par.lindkvist@kentor.se>
2017-10-06 10:47:14 +0200

    Lade till ett väldigt enkelt test och en tillhörande in-memory-db.

---
+++ src/test/java/se/skltp/loghandler/LogpostParserServiceTest.java
@@ -0,0 +1,31 @@
+package se.skltp.loghandler;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import se.skltp.loghandler.models.Anslutning;
+
+import java.util.List;
+
+/**
+ * Created by parlin on 2017-10-06.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class LogpostParserServiceTest {
+
+    @Autowired
+    private LogpostParserService logpostParserService;
+
+    @Test
+    public void parseLogpost() throws InterruptedException {
+        logpostParserService.parseLogpost("Test");
+        List<Anslutning> latestAnlutningar = LogpostParserService.getLatestAnlutningar();
+        Assert.assertEquals(0, latestAnlutningar.size());
+    }
+
+}
